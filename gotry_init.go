65577cb51a31526d9f7db2c3dfd7f0418276b522
Jenazads <felipe.moreno.vera@gmail.com>
2018-07-26 04:59:44 -0500

    init gotry

--- gotry_init.go
+++ gotry_init.go
@@ -3,7 +3,6 @@ package gotry
 import "fmt"
 
 func init(){
-  fmt.Println("\n\n====> GoTry version 1.0.3 last update 14/04/2018 \n")
-  fmt.Println("====> Init the GoTry Catch/Finally Library in Golang :D\n")
-  fmt.Println("====> by Jenazads.\n\n")
+  fmt.Println("====> Init GoTry, an Error/Panic Handling Library :D")
+  fmt.Println("====> by Jenazads.\n")
 }
