162e3f5705b3a5cecd8542c6b56412e499cda98e
Jenazads <felipe.moreno.vera@gmail.com>
2018-08-21 07:16:05 -0500

    init

--- gotry_init.go
+++ gotry_init.go
@@ -1,8 +1,10 @@
 package gotry
 
-import "fmt"
+import(
+//  "fmt";
+)
 
 func init(){
-  fmt.Println("====> Init GoTry, an Error/Panic Handling Library :D")
-  fmt.Println("====> by Jenazads.\n")
+  //fmt.Println("====> Init GoTry, an Error/Panic Handling Library :D")
+  //fmt.Println("====> by Jenazads.\n")
 }
